68afa36e755b827a183430d8f4796df6eb1f2d58
CottonTesseract <98365960+CottonTesseract@users.noreply.github.com>
2022-01-27 13:10:58 +0900

    fix index.php in login Please make indent!!!

--- login/index.php
+++ login/index.php
@@ -14,41 +14,42 @@ header('Location: contents/index.php');
  ?>
 
 <!DOCTYPE html>
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width,initial-scale=1.0">
-<title>ログイン</title>
-<link rel="stylesheet" href="loginpage.css">
-</head>
-<body>
-<img src="logo.png" height="60">
-<hr>
-<span class="infofont">
-ようこそ！<br>
-IDと認証番号を入力し、<br>
-「ログイン」ボタンを押してください。
-<hr>
-</span>
-<form action="login.php" method="post">
-<table>
-<tr>
-<td>
-<table>
-<tr>
-<td>ID</td>
-<td><input type="number" name="userid" class="loginform"></td>
-</tr>
-<tr>
-<td>認証番号</td>
-<td><input type="number" name="pass" class="loginform"></td>
-</tr>
-</table>
-</td>
-<td class="loginbuttontd"><button type="submit" class="submitbutton">ログイン</button></td>
-</tr>
-</table>
-</form>
-<hr>
-&#169;2022 Komenezumi
-</body>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width,initial-scale=1.0">
+		<title>ログイン</title>
+		<link rel="stylesheet" href="loginpage.css">
+	</head>
+	<body>
+		<img src="logo.png" height="60">
+		<hr>
+		<span class="infofont">
+			ようこそ!<br>
+			IDと認証番号を入力し、<br>
+			「ログイン」ボタンを押してください。
+			<hr>
+		</span>
+		<form action="login.php" method="post">
+			<table>
+				<tr>
+					<td>
+						<table>
+							<tr>
+								<td>ID</td>
+								<td><input type="number" name="userid" class="loginform"></td>
+							</tr>
+							<tr>
+								<td>認証番号</td>
+								<td><input type="number" name="pass" class="loginform"></td>
+							</tr>
+						</table>
+					</td>
+					<td class="loginbuttontd"><button type="submit" class="submitbutton">ログイン</button></td>
+				</tr>
+			</table>
+		</form>
+		<hr>
+		&#169;2022 Komenezumi
+	</body>
 </html>
