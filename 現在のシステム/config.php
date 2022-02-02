dabd425f1f4490974e0af9a5212ee2eadf3458e8
Yuto-Nakagoshi <98809368+Yuto-Nakagoshi@users.noreply.github.com>
2022-02-03 08:34:56 +0900

    整理整頓しました

--- 現在のシステム/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-//ini_set('display_errors', 1);
-
-define('DSN', 'mysql:host=mysql7.star.ne.jp;dbname=komedora_testdb');
-define('DB_USER', 'komedora_users');
-define('DB_PASS', 'komenezumi1006');
--- 現在のシステム/アーカイブ/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-//ini_set('display_errors', 0);
-
-define('DSN', 'mysql:host=sv66.star.ne.jp;dbname=komedora_testdb');
-define('DB_USER', 'komedora_users');
-define('DB_PASS', 'komenezumi1006');
